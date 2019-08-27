82d121abf4f8f2faa5cb88f6bbb8f45bc94a9285
Ptashkina Liliya <efekta9@gmail.com>
2019-08-27 19:00:20 +0300

    type text js

--- build/static/js/main.js
+++ build/static/js/main.js
@@ -6,9 +6,9 @@ $(document).ready(function () {
 $(function(){
     $(".element").typed({
         // strings: ["Lorem 1 ^1000", "Lorem 2 ^1000", "Lorem 3 ^1000"],
-        typeSpeed: 20,
+        typeSpeed: 50,
         backSpeed: 100,
-        startDelay: 1000,
+        startDelay: 1500,
         loop: true,
         loopCount: false,
         stringsElement: $('#typed-strings')
