6df48e157539d8603f682055dd0ce6a38f7c273f
Ptashkina Liliya <efekta9@gmail.com>
2019-08-27 15:59:58 +0300

    верстка
    навбар
    печатается текст на css3

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -7,13 +7,19 @@ let uglify = require('gulp-uglify'),
 
 module.exports = function () {
     $.gulp.task('libsJS:dev', () => {
-        return $.gulp.src(['node_modules/svg4everybody/dist/svg4everybody.min.js'])
+        return $.gulp.src([
+            'node_modules/svg4everybody/dist/svg4everybody.min.js',
+            'dev/libs/fontawesome/svg-with-js/js/fontawesome-all.js'
+            ])
             .pipe(concat('libs.min.js'))
             .pipe($.gulp.dest(scriptsPATH.ouput));
     });
 
     $.gulp.task('libsJS:build', () => {
-        return $.gulp.src(['node_modules/svg4everybody/dist/svg4everybody.min.js'])
+        return $.gulp.src([
+            'node_modules/svg4everybody/dist/svg4everybody.min.js',
+            'dev/libs/fontawesome/svg-with-js/js/fontawesome-all.js'
+            ])
             .pipe(concat('libs.min.js'))
             .pipe(uglify())
             .pipe($.gulp.dest(scriptsPATH.ouput));
